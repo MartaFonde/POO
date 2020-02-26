cb22a48596a5c2c4d513cb9ebfaff83c99e41fd4
Marta Fondevila <55930814+MartaFonde@users.noreply.github.com>
2020-02-26 19:13:12 +0100

    Update Circunferencia.java

--- GeometriaExcepHerencia/src/geometria/Circunferencia.java
+++ GeometriaExcepHerencia/src/geometria/Circunferencia.java
@@ -5,15 +5,28 @@ public class Circunferencia extends Figura {
     //como Figura implements InterfazUsuario, Circunferencia tb lo implementa
     private double radio;
 
+    /**
+     * Inicializa Punto de origen, radio y nombre
+     * @param centro punto de origen
+     * @param radio radio
+     */
     public Circunferencia(Punto centro, double radio){
         super(centro, "circunferencia");
         setRadio(radio);
     }
 
+    /**
+     * Inicializa el punto de origen a (0,0) y radio a 1
+     */
     public Circunferencia(){
         this(new Punto(), 1);
     }
 
+    /**
+     * Establece la propiedad radio mayor o igual a cero
+     * @param radio radio
+     * @throws ArithmeticException si el radio es negativo 
+     */
     public void setRadio(double radio) throws ArithmeticException{
         if(radio<0){
             throw new ArithmeticException("El radio no puede ser negativo");
@@ -21,6 +34,9 @@ public class Circunferencia extends Figura {
         this.radio=radio; 
     }
 
+    /**
+     * Pide nombre, punto de origen y radio al usuario
+     */
     @Override
     public void pedirDatos(){
         boolean error;
@@ -37,6 +53,9 @@ public class Circunferencia extends Figura {
         }while(error);
     }
 
+    /**
+     * Muestra nombre, punto de origen y radio
+     */
     @Override
     public void mostrarDatos(){
         super.mostrarDatos();
